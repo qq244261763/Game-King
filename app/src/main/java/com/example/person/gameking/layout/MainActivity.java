ae0d966af281363f4d6349305cc0f1feff60bfd0
Wan <wan.wang@huiche51.com>
2018-03-01 16:06:41 +0800

    gradle4.5

--- app/src/main/java/com/example/person/gameking/layout/MainActivity.java
+++ app/src/main/java/com/example/person/gameking/layout/MainActivity.java
@@ -1,7 +1,9 @@
-package com.example.person.gameking;
+package com.example.person.gameking.layout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.example.person.gameking.R;
 
 public class MainActivity extends AppCompatActivity {
 
