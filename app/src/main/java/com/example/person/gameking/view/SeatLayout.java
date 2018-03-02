124cb1a3f60a7daedf5e1f5ad5b05a40966a9c58
Wan <wan.wang@huiche51.com>
2018-03-02 17:14:08 +0800

    调整一下包结构

--- app/src/main/java/com/example/person/gameking/view/SeatLayout.java
+++ app/src/main/java/com/example/person/gameking/view/SeatLayout.java
@@ -1,10 +1,9 @@
-package com.example.person.gameking.base.view;
+package com.example.person.gameking.view;
 
 import android.content.Context;
 import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.Log;
-import android.util.TypedValue;
 import android.widget.RelativeLayout;
 
 import com.example.person.gameking.R;
@@ -16,18 +15,17 @@ public class SeatLayout extends RelativeLayout {
 
     public SeatLayout(Context context, AttributeSet attrs) {
         super(context, attrs,0);
-        init(context,attrs,0);
+        init(context,attrs);
 
     }
 
     public SeatLayout(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
-
     }
 
 
-    private void init(Context context,AttributeSet attrs, int defStyleAttr) {
-        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.SearchView, defStyleAttr, 0);
+    private void init(Context context,AttributeSet attrs) {
+        TypedArray ta = context.obtainStyledAttributes(attrs, R.styleable.SearchView);
         boolean isOpponent = ta.getBoolean(R.styleable.SeatLayout_opponent,false);
         ta.recycle();
         Log.e("isOpponent",isOpponent+"");
